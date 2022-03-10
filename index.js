cac5af9f28200424bd9d2c2eae3028fadc116585
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-03-10 11:59:16 +0200

    calendar

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //write js here
+
+function onLoad() {
+    //can put more than one thing
+    displayDaysOfTheMonth()
+}
+window.onload = onLoad()
+const daysCount = 31; //Assumption always display 31 days
+function displayDaysOfTheMonth() {
+    //1) Get the div where we want to add the days as children: the parent
+    let monthNode = document.getElementById("month");
+
+    for (let day = 1; day <= daysCount; day++) {
+        //2) create a new div
+        let dayNode = document.createElement("div")
+        //3) add the text/class (we customize it)
+        dayNode.innerText = day
+        dayNode.classList.add("day");
+    }
+
+    //4) appent it to the parent
+
+}
